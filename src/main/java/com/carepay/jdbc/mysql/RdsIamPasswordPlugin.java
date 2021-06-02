86afb7be2338149fbbdbd69cc46bbc6cfc714058
Barry Lagerweij <b.lagerweij@carepay.co.ke>
2021-06-02 15:29:41 +0200

    added spotless

--- src/main/java/com/carepay/jdbc/mysql/RdsIamPasswordPlugin.java
+++ src/main/java/com/carepay/jdbc/mysql/RdsIamPasswordPlugin.java
@@ -52,102 +52,109 @@ import com.mysql.cj.util.StringUtils;
 
 import static java.time.ZoneOffset.UTC;
 
-/**
- * MySQL Clear Password Authentication Plugin
- */
+/** MySQL Clear Password Authentication Plugin */
 public class RdsIamPasswordPlugin implements AuthenticationPlugin<NativePacketPayload> {
-    /** replaces the built-in clear-text plugin */
-    public static String PLUGIN_NAME = "mysql_clear_password";
+  /** replaces the built-in clear-text plugin */
+  private static final String PLUGIN_NAME = "mysql_clear_password";
 
-    private Protocol<NativePacketPayload> protocol;
+  private Protocol<NativePacketPayload> protocol;
 
-    private final Clock clock;
+  private final Clock clock;
 
-    private final RdsAWS4Signer signer;
-    private LocalDateTime expiryDate;
-    protected String username;
-    private String password;
+  private final RdsAWS4Signer signer;
+  private LocalDateTime expiryDate;
+  protected String username;
+  private String password;
 
-    /**
-     * Default constructor, uses AWS provider-chain by default
-     */
-    public RdsIamPasswordPlugin() {
-        this(new DefaultCredentialsProviderChain(), new DefaultRegionProviderChain(), Clock.systemUTC());
+  /** Default constructor, uses AWS provider-chain by default */
+  public RdsIamPasswordPlugin() {
+    this(
+        new DefaultCredentialsProviderChain(), new DefaultRegionProviderChain(), Clock.systemUTC());
+  }
+
+  /**
+   * Custom constructor, used for tests.
+   *
+   * @param credentialsProvider the AWS credentials-provider to use
+   * @param regionProvider the AWS region-provider to use
+   * @param clock the system-clock used to check if token is expired
+   */
+  public RdsIamPasswordPlugin(
+      final CredentialsProvider credentialsProvider,
+      final RegionProvider regionProvider,
+      final Clock clock) {
+    this.clock = clock;
+    this.signer = new RdsAWS4Signer(credentialsProvider, regionProvider, this.clock);
+    PemKeyStoreProvider.register();
+  }
+
+  @Override
+  public void init(Protocol<NativePacketPayload> prot, MysqlCallbackHandler cbh) {
+    this.protocol = prot;
+    final PropertySet propertySet = this.protocol.getPropertySet();
+    propertySet.getBooleanProperty(PropertyKey.useSSL).setValue(true);
+    propertySet.getBooleanProperty(PropertyKey.verifyServerCertificate).setValue(true);
+    ((EnumProperty<PropertyDefinitions.SslMode>) propertySet.getEnumProperty(PropertyKey.sslMode))
+        .setValue(PropertyDefinitions.SslMode.VERIFY_CA);
+    propertySet
+        .getStringProperty(PropertyKey.trustCertificateKeyStoreUrl)
+        .setValue("classpath:/rds-combined-ca-bundle.pem");
+    propertySet.getStringProperty(PropertyKey.trustCertificateKeyStoreType).setValue("PEM");
+    propertySet.getStringProperty(PropertyKey.enabledTLSProtocols).setValue("TLSv1.2");
+  }
+
+  public void destroy() {}
+
+  public String getProtocolPluginName() {
+    return PLUGIN_NAME;
+  }
+
+  public boolean requiresConfidentiality() {
+    return true;
+  }
+
+  public boolean isReusable() {
+    return true;
+  }
+
+  public void setAuthenticationParameters(String user, String password) {
+    this.username = user;
+  }
+
+  public boolean nextAuthenticationStep(
+      NativePacketPayload fromServer, List<NativePacketPayload> toServer) {
+    toServer.clear();
+    final String token = getPassword();
+    final String encoding =
+        this.protocol.versionMeetsMinimum(5, 7, 6)
+            ? this.protocol.getPasswordCharacterEncoding()
+            : "UTF-8";
+    final NativePacketPayload payload =
+        new NativePacketPayload(StringUtils.getBytes(token, encoding));
+    payload.setPosition(payload.getPayloadLength());
+    payload.writeInteger(IntegerDataType.INT1, 0);
+    payload.setPosition(0);
+    toServer.add(payload);
+    return true;
+  }
+
+  /**
+   * Gets the IAM token. Creates a new token when the token is expired.
+   *
+   * @return the IAM RDS token.
+   */
+  public String getPassword() {
+    refreshToken();
+    return this.password;
+  }
+
+  protected void refreshToken() {
+    final LocalDateTime now = LocalDateTime.ofInstant(clock.instant(), UTC);
+    if (this.expiryDate == null || expiryDate.isBefore(now)) {
+      final String host = this.protocol.getSocketConnection().getHost();
+      final int port = this.protocol.getSocketConnection().getPort();
+      this.password = signer.generateToken(host, port, username);
+      this.expiryDate = now.plusMinutes(10L); // Token expires after 15 min, so refresh after 10 min
     }
-
-    /**
-     * Custom constructor, used for tests.
-     *
-     * @param credentialsProvider the AWS credentials-provider to use
-     * @param regionProvider the AWS region-provider to use
-     * @param clock the system-clock used to check if token is expired
-     */
-    public RdsIamPasswordPlugin(final CredentialsProvider credentialsProvider, final RegionProvider regionProvider, final Clock clock) {
-        this.clock = clock;
-        this.signer = new RdsAWS4Signer(credentialsProvider, regionProvider, this.clock);
-        PemKeyStoreProvider.register();
-    }
-
-    @Override
-    public void init(Protocol<NativePacketPayload> prot, MysqlCallbackHandler cbh) {
-        this.protocol = prot;
-        final PropertySet propertySet = this.protocol.getPropertySet();
-        propertySet.getBooleanProperty(PropertyKey.useSSL).setValue(true);
-        propertySet.getBooleanProperty(PropertyKey.verifyServerCertificate).setValue(true);
-        ((EnumProperty<PropertyDefinitions.SslMode>) propertySet.getEnumProperty(PropertyKey.sslMode)).setValue(PropertyDefinitions.SslMode.VERIFY_CA);
-        propertySet.getStringProperty(PropertyKey.trustCertificateKeyStoreUrl).setValue("classpath:/rds-combined-ca-bundle.pem");
-        propertySet.getStringProperty(PropertyKey.trustCertificateKeyStoreType).setValue("PEM");
-        propertySet.getStringProperty(PropertyKey.enabledTLSProtocols).setValue("TLSv1.2");
-    }
-
-    public void destroy() {
-    }
-
-    public String getProtocolPluginName() {
-        return PLUGIN_NAME;
-    }
-
-    public boolean requiresConfidentiality() {
-        return true;
-    }
-
-    public boolean isReusable() {
-        return true;
-    }
-
-    public void setAuthenticationParameters(String user, String password) {
-        this.username = user;
-    }
-
-    public boolean nextAuthenticationStep(NativePacketPayload fromServer, List<NativePacketPayload> toServer) {
-        toServer.clear();
-        final String token = getPassword();
-        final String encoding = this.protocol.versionMeetsMinimum(5, 7, 6) ? this.protocol.getPasswordCharacterEncoding() : "UTF-8";
-        final NativePacketPayload payload = new NativePacketPayload(StringUtils.getBytes(token, encoding));
-        payload.setPosition(payload.getPayloadLength());
-        payload.writeInteger(IntegerDataType.INT1, 0);
-        payload.setPosition(0);
-        toServer.add(payload);
-        return true;
-    }
-
-    /**
-     * Gets the IAM token. Creates a new token when the token is expired.
-     *
-     * @return the IAM RDS token.
-     */
-    public String getPassword() {
-        refreshToken();
-        return this.password;
-    }
-
-    protected void refreshToken() {
-        final LocalDateTime now = LocalDateTime.ofInstant(clock.instant(), UTC);
-        if (this.expiryDate == null || expiryDate.isBefore(now)) {
-            final String host = this.protocol.getSocketConnection().getHost();
-            final int port = this.protocol.getSocketConnection().getPort();
-            this.password = signer.generateToken(host, port, username);
-            this.expiryDate = now.plusMinutes(10L); // Token expires after 15 min, so refresh after 10 min
-        }
-    }
+  }
 }
--- src/test/java/com/carepay/jdbc/mysql/RdsIamPasswordPluginTest.java
+++ src/test/java/com/carepay/jdbc/mysql/RdsIamPasswordPluginTest.java
@@ -29,74 +29,88 @@ import static org.mockito.Mockito.when;
 
 class RdsIamPasswordPluginTest {
 
-    private Clock clock = Clock.fixed(Instant.parse("2021-10-12T12:13:14Z"), ZoneId.of("UTC"));
-    private RdsIamPasswordPlugin plugin;
-    private Protocol<NativePacketPayload> protocol;
-    private DefaultPropertySet propertySet;
+  private Clock clock = Clock.fixed(Instant.parse("2021-10-12T12:13:14Z"), ZoneId.of("UTC"));
+  private RdsIamPasswordPlugin plugin;
+  private Protocol<NativePacketPayload> protocol;
+  private DefaultPropertySet propertySet;
 
-    @BeforeEach
-    void setUp() {
-        plugin = new RdsIamPasswordPlugin(() -> new Credentials("ABC", "DEF", "GHI"), () -> "eu-west-1", clock);
-        //noinspection unchecked
-        protocol = mock(Protocol.class);
-        propertySet = new DefaultPropertySet();
-        when(protocol.getPropertySet()).thenReturn(propertySet);
-        when(protocol.getSocketConnection()).thenReturn(new FakeSocketConnection("cp-ke-test-fake-db.cluster-c3u8jposmdov.eu-west-1.rds.amazonaws.com", 3306));
+  @BeforeEach
+  void setUp() {
+    plugin =
+        new RdsIamPasswordPlugin(
+            () -> new Credentials("ABC", "DEF", "GHI"), () -> "eu-west-1", clock);
+    //noinspection unchecked
+    protocol = mock(Protocol.class);
+    propertySet = new DefaultPropertySet();
+    when(protocol.getPropertySet()).thenReturn(propertySet);
+    when(protocol.getSocketConnection())
+        .thenReturn(
+            new FakeSocketConnection(
+                "cp-ke-test-fake-db.cluster-c3u8jposmdov.eu-west-1.rds.amazonaws.com", 3306));
+  }
+
+  @Test
+  void testInit() {
+    plugin.init(protocol, null);
+    assertThat(propertySet.getEnumProperty(PropertyKey.sslMode).getValue())
+        .isEqualTo(PropertyDefinitions.SslMode.VERIFY_CA);
+    assertThat(propertySet.getStringProperty(PropertyKey.trustCertificateKeyStoreType).getValue())
+        .isEqualTo("PEM");
+  }
+
+  @Test
+  void getProtocolPluginName() {
+    assertThat(plugin.getProtocolPluginName()).isEqualTo("mysql_clear_password");
+  }
+
+  @Test
+  void requiresConfidentiality() {
+    assertThat(plugin.requiresConfidentiality()).isTrue();
+  }
+
+  @Test
+  void isReusable() {
+    assertThat(plugin.isReusable()).isTrue();
+  }
+
+  @Test
+  void setAuthentication() {
+    plugin.setAuthenticationParameters("johndoe", "secret");
+    assertThat(plugin.username).isEqualTo("johndoe");
+  }
+
+  @Test
+  void nextAuthenticationStep() {
+    plugin.init(protocol, null);
+    NativePacketPayload fromServer = new NativePacketPayload(2000);
+    List<NativePacketPayload> toServer = new ArrayList<>();
+    plugin.nextAuthenticationStep(fromServer, toServer);
+    assertThat(toServer).isNotEmpty();
+    NativePacketPayload loginPacket = toServer.get(0);
+    String str = new String(loginPacket.getByteBuffer(), 0, loginPacket.getPayloadLength() - 1);
+    assertThat(str)
+        .isEqualTo(
+            "cp-ke-test-fake-db.cluster-c3u8jposmdov.eu-west-1.rds.amazonaws.com:3306/?Action=connect&DBUser=null&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ABC%2F20211012%2Feu-west-1%2Frds-db%2Faws4_request&X-Amz-Date=20211012T121314Z&X-Amz-Expires=900&X-Amz-Security-Token=GHI&X-Amz-SignedHeaders=host&X-Amz-Signature=c51622f22daffcfaa6707878fb009853f3764ec02246dd2d0a2c9e0c1c6bad37");
+  }
+
+  static class FakeSocketConnection extends AbstractSocketConnection {
+
+    public FakeSocketConnection(final String host, final int port) {
+      this.host = host;
+      this.port = port;
     }
 
-    @Test
-    void testInit() {
-        plugin.init(protocol, null);
-        assertThat(propertySet.getEnumProperty(PropertyKey.sslMode).getValue()).isEqualTo(PropertyDefinitions.SslMode.VERIFY_CA);
-        assertThat(propertySet.getStringProperty(PropertyKey.trustCertificateKeyStoreType).getValue()).isEqualTo("PEM");
-    }
+    @Override
+    public void connect(
+        String host,
+        int port,
+        PropertySet propertySet,
+        ExceptionInterceptor exceptionInterceptor,
+        Log log,
+        int loginTimeout) {}
 
-    @Test
-    void getProtocolPluginName() {
-        assertThat(plugin.getProtocolPluginName()).isEqualTo("mysql_clear_password");
-    }
-
-    @Test
-    void requiresConfidentiality() {
-        assertThat(plugin.requiresConfidentiality()).isTrue();
-    }
-
-    @Test
-    void isReusable() {
-        assertThat(plugin.isReusable()).isTrue();
-    }
-
-    @Test
-    void setAuthentication() {
-        plugin.setAuthenticationParameters("johndoe", "secret");
-        assertThat(plugin.username).isEqualTo("johndoe");
-    }
-
-    @Test
-    void nextAuthenticationStep() {
-        plugin.init(protocol, null);
-        NativePacketPayload fromServer = new NativePacketPayload(2000);
-        List<NativePacketPayload> toServer = new ArrayList<>();
-        plugin.nextAuthenticationStep(fromServer, toServer);
-        assertThat(toServer).isNotEmpty();
-        NativePacketPayload loginPacket = toServer.get(0);
-        String str = new String(loginPacket.getByteBuffer(),0, loginPacket.getPayloadLength()-1);
-        assertThat(str).isEqualTo("cp-ke-test-fake-db.cluster-c3u8jposmdov.eu-west-1.rds.amazonaws.com:3306/?Action=connect&DBUser=null&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ABC%2F20211012%2Feu-west-1%2Frds-db%2Faws4_request&X-Amz-Date=20211012T121314Z&X-Amz-Expires=900&X-Amz-Security-Token=GHI&X-Amz-SignedHeaders=host&X-Amz-Signature=c51622f22daffcfaa6707878fb009853f3764ec02246dd2d0a2c9e0c1c6bad37");
-    }
-
-    static class FakeSocketConnection extends AbstractSocketConnection {
-
-        public FakeSocketConnection(final String host, final int port) {
-            this.host = host;
-            this.port = port;
-        }
-
-        @Override
-        public void connect(String host, int port, PropertySet propertySet, ExceptionInterceptor exceptionInterceptor, Log log, int loginTimeout) {
-        }
-
-        @Override
-        public void performTlsHandshake(ServerSession serverSession) throws SSLParamsException, FeatureNotAvailableException, IOException {
-        }
-    }
+    @Override
+    public void performTlsHandshake(ServerSession serverSession)
+        throws SSLParamsException, FeatureNotAvailableException, IOException {}
+  }
 }
